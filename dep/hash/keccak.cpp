5300a400560fdeef156ecad50264835e60290c9d
ugo <ugovaretto@gmail.com>
2022-12-03 09:55:24 +0800

    Replace endian.h with sys/types.h to make it portable between Linux and MacOS

--- dep/hash/keccak.cpp
+++ dep/hash/keccak.cpp
@@ -8,11 +8,7 @@
 
 // big endian architectures need #define __BYTE_ORDER __BIG_ENDIAN
 #ifndef _MSC_VER
-#ifdef __APPLE__
-#include <machine/endian.h>
-#else
-#include <endian.h>
-#endif
+#include <sys/types.h>
 #endif
 // big endian architectures need #define __BYTE_ORDER __BIG_ENDIAN
 
