b973b907af9739207f431bf2dc97fba8fc78efdf
Ugo Varetto <ugovaretto@gmail.com>
2023-05-14 15:34:41 +0800

    updated script

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-git clone --recurse-submodules https://github.com/uv-cpp/s3client.git repo
+if [-d repo]
+then
+  cd repo
+  git pull
+else
+  git clone --recurse-submodules https://github.com/uv-cpp/s3client.git repo
+fi
 doxygen repo/doc/Doxyfile.ghpages > doxygen.log 2> doxygen.errors
+git add -A
+git commit -m "Generated"
 git push origin gh-pages
 
 
