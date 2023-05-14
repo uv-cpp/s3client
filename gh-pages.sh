db74167be40014ebde092d41474d5b4a36a56521
Ugo Varetto <ugovaretto@gmail.com>
2023-05-14 15:24:03 +0800

    .gitignore

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git clone --recurse-submodules https://github.com/uv-cpp/s3client.git repo
-doxygen repo/doc/Doxyfile.ghpages
+doxygen repo/doc/Doxyfile.ghpages > doxygen.log 2> doxygen.errors
 git push origin gh-pages
 
 
