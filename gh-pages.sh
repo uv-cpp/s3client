803a9fdf9fd9cd8cfed92a302bcdd2edc1bf3a27
ugo <ugovaretto@gmail.com>
2023-05-15 18:01:33 +0800

    Generated

--- gh-pages.sh
+++ gh-pages.sh
@@ -6,12 +6,21 @@ if [[ -d "./repo" ]]; then
 else
   git clone --recurse-submodules https://github.com/uv-cpp/s3client.git repo
 fi
-cd repo/doc
-echo "Generating Doxygen documentation..."
 if [[ -d doxygen-docs ]]; then
   echo "  deleting previously generated documentation..."
   rm -rf doxygen-docs
 fi
+if [[ -d build ]]; then
+  rm -rf build
+fi
+if [[ -d doc ]]; then
+  rm -rf doc
+fi
+if [[ -d .cache ]]; then
+  rm -rf .cache
+fi
+cd repo/doc
+echo "Generating Doxygen documentation..."
 echo "  running doxygen..."
 doxygen Doxyfile.ghpages > doxygen.log 2> doxygen.errors
 echo "Pushing to gh-pages..."
