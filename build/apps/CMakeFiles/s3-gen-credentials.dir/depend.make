e48c8f9b31c88f95069bb1ab8c1e605396e2bc4d
Ugo Varetto <ugovaretto@gmail.com>
2023-05-17 21:18:02 +0800

    ...

--- build/apps/CMakeFiles/s3-gen-credentials.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for s3-gen-credentials.
-# This may be replaced when dependencies are built.
--- gh-pages.sh
+++ gh-pages.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
-if [[ -d "./repo" ]]; then
+if [[ -d repo ]]; then
   cd repo
   git pull
   cd ..
 else
   git clone --recurse-submodules https://github.com/uv-cpp/s3client.git repo
 fi
+if [[ -d doxygen-docs ]]; then
+  echo "  deleting previously generated documentation..."
+  rm -rf doxygen-docs
+fi
+# if [[ -d build ]]; then
+#   rm -rf build
+# fi
+# if [[ -d doc ]]; then
+#   rm -rf doc
+# fi
+# if [[ -d .cache ]]; then
+#   rm -rf .cache
+# fi
 cd repo/doc
 echo "Generating Doxygen documentation..."
+echo "  running doxygen..."
 doxygen Doxyfile.ghpages > doxygen.log 2> doxygen.errors
 echo "Pushing to gh-pages..."
 cd ../../
 git add -A
-git commit -m "Generated"
-git push origin gh-pages
+git commit -m "Generated" #> /dev/null 2>&1
+git push origin gh-pages #> /dev/null 2>&1
 echo "...done"
-#rm -rf doxygen-docs
-
-
