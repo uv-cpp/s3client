bea8c4f2e87667045e418ac12d0a7e5ed8a20349
ugovaretto <ugovaretto@gmail.com>
2023-08-21 19:02:52 +0800

    Generated

--- doxygen-docs/html/navtreedata.js
+++ doxygen-docs/html/navtreedata.js
@@ -30,22 +30,22 @@ var NAVTREE =
       [ "Configuration", "md_docs_2apitest.html#autotoc_md16", null ]
     ] ],
     [ "Implementing custom requests", "md_docs_2custom__request.html", [
-      [ "Example: Bucket tagging", "md_docs_2custom__request.html#autotoc_md18", [
-        [ "<tt>TagBucket</tt>: PutBucketTagging request", "md_docs_2custom__request.html#autotoc_md19", null ],
-        [ "<tt>BucketTags</tt>: GetBucketTagging response", "md_docs_2custom__request.html#autotoc_md20", null ]
+      [ "Example: Bucket tagging", "md_docs_2custom__request.html#autotoc_md27", [
+        [ "TagBucket: PutBucketTagging request", "md_docs_2custom__request.html#autotoc_md28", null ],
+        [ "<tt>BucketTags</tt>: GetBucketTagging response", "md_docs_2custom__request.html#autotoc_md29", null ]
       ] ]
     ] ],
     [ "Presign", "md_docs_2presign.html", [
-      [ "Example: Upload object", "md_docs_2presign.html#autotoc_md22", [
-        [ "Presign URL", "md_docs_2presign.html#autotoc_md23", null ],
-        [ "Upload object", "md_docs_2presign.html#autotoc_md24", null ],
-        [ "Verify upload", "md_docs_2presign.html#autotoc_md25", null ]
+      [ "Example: Upload object", "md_docs_2presign.html#autotoc_md18", [
+        [ "Presign URL", "md_docs_2presign.html#autotoc_md19", null ],
+        [ "Upload object", "md_docs_2presign.html#autotoc_md20", null ],
+        [ "Verify upload", "md_docs_2presign.html#autotoc_md21", null ]
       ] ]
     ] ],
     [ "S3 Actions", "md_docs_2s3-actions.html", [
-      [ "Implemented (26)", "md_docs_2s3-actions.html#autotoc_md27", null ],
-      [ "In progress (4)", "md_docs_2s3-actions.html#autotoc_md28", null ],
-      [ "All (133)", "md_docs_2s3-actions.html#autotoc_md29", null ]
+      [ "Implemented (26)", "md_docs_2s3-actions.html#autotoc_md24", null ],
+      [ "In progress (4)", "md_docs_2s3-actions.html#autotoc_md25", null ],
+      [ "All (133)", "md_docs_2s3-actions.html#autotoc_md26", null ]
     ] ],
     [ "Modules", "modules.html", "modules" ],
     [ "Data Structures", "annotated.html", [
