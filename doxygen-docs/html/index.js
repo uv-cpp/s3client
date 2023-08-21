b0cf506acd4a166730353f8398dc33e298c7abef
ugovaretto <ugovaretto@gmail.com>
2023-08-21 19:04:25 +0800

    Generated

--- doxygen-docs/html/index.js
+++ doxygen-docs/html/index.js
@@ -1,21 +1,21 @@
 var index =
 [
     [ "Library", "index.html#autotoc_md0", null ],
-    [ "Applications", "index.html#autotoc_md1", null ],
-    [ "Build and install", "index.html#autotoc_md2", [
-      [ "Compilation options", "index.html#autotoc_md3", null ]
+    [ "Applications", "index.html#autotoc_md3", null ],
+    [ "Build and install", "index.html#autotoc_md5", [
+      [ "Compilation options", "index.html#autotoc_md8", null ]
     ] ],
-    [ "Test", "index.html#autotoc_md4", null ],
-    [ "Parallel data transfer", "index.html#autotoc_md5", [
-      [ "Lustre", "index.html#autotoc_md6", null ],
-      [ "Others", "index.html#autotoc_md7", null ]
+    [ "Test", "index.html#autotoc_md10", null ],
+    [ "Parallel data transfer", "index.html#autotoc_md16", [
+      [ "Lustre", "index.html#autotoc_md17", null ],
+      [ "Others", "index.html#autotoc_md21", null ]
     ] ],
-    [ "Sending S3 requests", "index.html#autotoc_md8", [
-      [ "List bucket content", "index.html#autotoc_md9", null ],
-      [ "Extract bytes 100-150 from object", "index.html#autotoc_md10", null ],
-      [ "Store data into object", "index.html#autotoc_md11", null ]
+    [ "Sending S3 requests", "index.html#autotoc_md23", [
+      [ "List bucket content", "index.html#autotoc_md24", null ],
+      [ "Extract bytes 100-150 from object", "index.html#autotoc_md25", null ],
+      [ "Store data into object", "index.html#autotoc_md26", null ]
     ] ],
-    [ "Parallel upload", "index.html#autotoc_md12", null ],
-    [ "Parallel file download", "index.html#autotoc_md13", null ],
-    [ "License", "index.html#autotoc_md14", null ]
+    [ "Parallel upload", "index.html#autotoc_md27", null ],
+    [ "Parallel file download", "index.html#autotoc_md28", null ],
+    [ "License", "index.html#autotoc_md29", null ]
 ];
--- doxygen-docs/html/navtreedata.js
+++ doxygen-docs/html/navtreedata.js
@@ -27,25 +27,25 @@ var NAVTREE =
   [ "SSS - S3 Toolkit", "index.html", [
     [ "S3 Toolkit", "index.html", "index" ],
     [ "API Test", "md_docs_2apitest.html", [
-      [ "Configuration", "md_docs_2apitest.html#autotoc_md16", null ]
+      [ "Configuration", "md_docs_2apitest.html#autotoc_md4", null ]
     ] ],
     [ "Implementing custom requests", "md_docs_2custom__request.html", [
-      [ "Example: Bucket tagging", "md_docs_2custom__request.html#autotoc_md27", [
-        [ "TagBucket: PutBucketTagging request", "md_docs_2custom__request.html#autotoc_md28", null ],
-        [ "<tt>BucketTags</tt>: GetBucketTagging response", "md_docs_2custom__request.html#autotoc_md29", null ]
+      [ "Example: Bucket tagging", "md_docs_2custom__request.html#autotoc_md7", [
+        [ "TagBucket: PutBucketTagging request", "md_docs_2custom__request.html#autotoc_md9", null ],
+        [ "<tt>BucketTags</tt>: GetBucketTagging response", "md_docs_2custom__request.html#autotoc_md11", null ]
       ] ]
     ] ],
     [ "Presign", "md_docs_2presign.html", [
       [ "Example: Upload object", "md_docs_2presign.html#autotoc_md18", [
         [ "Presign URL", "md_docs_2presign.html#autotoc_md19", null ],
         [ "Upload object", "md_docs_2presign.html#autotoc_md20", null ],
-        [ "Verify upload", "md_docs_2presign.html#autotoc_md21", null ]
+        [ "Verify upload", "md_docs_2presign.html#autotoc_md22", null ]
       ] ]
     ] ],
     [ "S3 Actions", "md_docs_2s3-actions.html", [
-      [ "Implemented (26)", "md_docs_2s3-actions.html#autotoc_md24", null ],
-      [ "In progress (4)", "md_docs_2s3-actions.html#autotoc_md25", null ],
-      [ "All (133)", "md_docs_2s3-actions.html#autotoc_md26", null ]
+      [ "Implemented (26)", "md_docs_2s3-actions.html#autotoc_md12", null ],
+      [ "In progress (4)", "md_docs_2s3-actions.html#autotoc_md13", null ],
+      [ "All (133)", "md_docs_2s3-actions.html#autotoc_md14", null ]
     ] ],
     [ "Modules", "modules.html", "modules" ],
     [ "Data Structures", "annotated.html", [
